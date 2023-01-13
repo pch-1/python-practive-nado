69cfef4412acb9e62e487b10b48b630b817c88e5
ChunghoPark <chungho1115@naver.com>
2023-01-13 10:51:33 +0900

    class quiz so easy, 다음은 예외처리

--- practice.py
+++ practice.py
@@ -835,3 +835,32 @@ class House:
         pass
 
 '''
+
+# Q 붕어빵 개수를 알 수 있는 방법이 있나?
+
+class House:
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    def show_detail(self):
+        print('self.location, self.house_type, self.deal_type, self.price, self.completion_year')
+        
+h1 = House('강남', '아파트', '매매', '10억', '2010년')
+h2 = House('마포', '오피스텔', '전세', '5억', '2007년')
+h3 = House('송파', '빌라', '월세', '50/500', '2000년')
+
+house_list = []
+
+house_list.append(h1)
+house_list.append(h2)
+house_list.append(h3)
+
+print('총 {}대의 매물이 있습니다'.format(len(house_list)))
+for house in house_list:
+    house.show_detail()
+    
+# [예외 처리]
